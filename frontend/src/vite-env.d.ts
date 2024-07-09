bff4abc84ff20d0a5962b9e0baeead48644eb8a5
Krzysztof Growiński <k.growinski@outlook.com>
2024-07-09 12:09:36 +0200

    chore: Update frontend styles and add initial backend configuration

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,19 +1,6 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_FIREBASE_API_KEY: string;
-  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
-  readonly VITE_FIREBASE_PROJECT_ID: string;
-  readonly VITE_FIREBASE_STORAGE_BUCKET: string;
-  readonly VITE_FIREBASE_MESSENGER_ID: string;
-  readonly VITE_FIREBASE_APP_ID: string;
-  readonly VITE_FIREBASE_MEASUREMENT_ID: string;
-  readonly VITE_API_URL: string;
-  readonly VITE_WS_URL: string;
-  readonly VITE_CURRENT_ENV: string;
-  readonly VITE_GOOGLE_MAP_API_KEY: string;
-  readonly VITE_OPEN_MAPS_API_URL: string;
-  readonly VITE_REACT_APP_MESSAGING_VALID_KEY: string;
   // more env variables...
 }
 
